bcdd662883faf003fccfcfc9f215616fe7ab0f98
pplociennik <pplociennik097@gmail.com>
2024-04-09 20:48:12 +0200

    [NO_JIRA] Added support for modInfo.

--- src/main/java/com/goaleaf/communities/GlfCommunitiesApplication.java
+++ src/main/java/com/goaleaf/communities/GlfCommunitiesApplication.java
@@ -1,14 +1,17 @@
 package com.goaleaf.communities;
 
 import com.github.pplociennik.commons.audit.AuditAwareImpl;
+import com.github.pplociennik.modinfo.config.ModuleInfoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing( auditorAwareRef = "auditAwareImpl" )
+@Import( value = { ModuleInfoConfiguration.class } )
 public class GlfCommunitiesApplication {
 
     public static void main( String[] args ) {
